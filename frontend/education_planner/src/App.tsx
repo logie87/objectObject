542435a866160842f4960308278b496830a6bfe2
ome-d <omelinedeffains@outlook.fr>
2025-11-08 11:09:35 -0700

    first ui version

---
+++ frontend/education_planner/src/App.tsx
@@ -0,0 +1,34 @@
+
+/*import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;*/
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
+
+const App: React.FC = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/home" element={<HomePage />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
+
